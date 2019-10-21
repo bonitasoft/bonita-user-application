6ef3a334df6409a434cfdb074adeccb8d5ab4f5b
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-10-21 14:09:16 +0200

    Update buildVersion.groovy

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -8,7 +8,7 @@ pipeline {
             steps {
                 sh "./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
                 withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
-                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=../target/bonita-user-application-${params.version}.bos" 
+                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./target/bonita-user-application-${params.version}.bos" 
                 }
                
             }

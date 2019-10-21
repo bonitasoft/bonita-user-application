0dff08220cf805603bbaa116918368cbc00f521b
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-10-21 14:11:38 +0200

    feat(release) upload release asset when building tag (#2)
    Closes BST-664

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -6,7 +6,11 @@ pipeline {
     stages {
         stage('Build and deploy') {
             steps {
-                sh("./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}")
+                sh "./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
+                withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
+                    sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./target/bonita-user-application-${params.version}.bos" 
+                }
+               
             }
         }
     }

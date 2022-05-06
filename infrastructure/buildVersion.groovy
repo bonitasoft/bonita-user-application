cfedac91382e7fd8881cb0e21542e9deb413ad6e
Baptiste Mesta <baptiste.mesta@gmail.com>
2022-05-06 11:42:18 +0200

    chore(build): use settings.xml from config file provider
    also update maven wrapper

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -6,7 +6,9 @@ pipeline {
     stages {
         stage('Build and deploy') {
             steps {
-                sh "./mvnw deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
+                configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
+                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}")
+                }
                 withCredentials([string(credentialsId: 'github-api', variable: 'GITHUB_API_TOKEN')]) {
                     sh "./infrastructure/upload-github-release-asset.sh github_api_token=$GITHUB_API_TOKEN tag=${params.version} filename=./target/bonita-user-application-${params.version}.bos" 
                 }
